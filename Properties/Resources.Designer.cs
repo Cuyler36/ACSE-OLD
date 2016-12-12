a325ede233a5cd5ceaa717e7366464c283dff7ba
Cuyler36 <olsenj96@yahoo.com>
2016-12-12 09:08:39 -0500

    Fix Invalid ResourceManager baseName

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace Animal_Crossing_GCN_Save_Editor.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Animal_Crossing_GCN_Save_Editor.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ACSE.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
